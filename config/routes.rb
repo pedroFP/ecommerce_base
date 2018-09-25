1e215b610f9148c90b7be64a3900edde9d4296a2
pedro_FP <pedro.araya.labra@gmail.com>
2018-09-24 22:34:52 -0300

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+
+  resources :products, only: :index do
+    resources :orders, only: :create
+  end
+
+  resources :orders, only: :index do
+  	collection do
+  		get 'clean'
+  	end
+  end
+
+  resources :billings , only: [:index] do
+    collection do
+        get 'pre_pay'
+        get 'execute'
+    end
+  end
+
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+  }
+
+  root 'products#index'
+
+end
